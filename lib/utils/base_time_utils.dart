b27dc0dc125095e0f8fd8b0487fd55291aba8815
kayoxu <371714752@qq.com>
2020-01-13 16:52:41 +0800

    修改日期选择器

--- lib/utils/base_time_utils.dart
+++ lib/utils/base_time_utils.dart
@@ -55,7 +55,7 @@ class BaseTimeUtils {
     now = now ?? DateTime.now();
 
     var dateTime =
-    DateTime(now.year, !start ? now.month + 1 : now.month, !start ? 0 : 1);
+        DateTime(now.year, !start ? now.month + 1 : now.month, !start ? 0 : 1);
 
     return getDateTime(start: start, dateTime: dateTime, plus: start ? 2 : 0);
   }
@@ -115,9 +115,7 @@ class BaseTimeUtils {
   * now的时间戳
   * */
   static String nowTimeStr({String format}) {
-    return timestampToTimeStr(DateTime
-        .now()
-        .millisecondsSinceEpoch,
+    return timestampToTimeStr(DateTime.now().millisecondsSinceEpoch,
         format: format);
   }
 
@@ -125,9 +123,7 @@ class BaseTimeUtils {
   * now的Timestamp
   * */
   static int nowTimestamp() {
-    return DateTime
-        .now()
-        .millisecondsSinceEpoch;
+    return DateTime.now().millisecondsSinceEpoch;
   }
 
   /*
@@ -223,12 +219,13 @@ class BaseTimeUtils {
   * start 是否是开始时间
   * 
   * */
-  static DateTime getDateTime({DateTime dateTime,
-    bool start,
-    int h = 0,
-    int m = 0,
-    int s = 0,
-    int plus = 0}) {
+  static DateTime getDateTime(
+      {DateTime dateTime,
+      bool start,
+      int h = 0,
+      int m = 0,
+      int s = 0,
+      int plus = 0}) {
     dateTime = dateTime ?? DateTime.now();
 
     if (null != start) {
