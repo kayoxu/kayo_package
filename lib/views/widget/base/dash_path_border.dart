b28db42c8d63c1793e2bb0b90ecbe51e57616f36
kayoxu <371714752@qq.com>
2020-01-20 16:16:11 +0800

    新增虚线

--- lib/views/widget/base/dash_path_border.dart
+++ lib/views/widget/base/dash_path_border.dart
@@ -87,3 +87,63 @@ class DashPathBorder extends Border {
         'A border can only be drawn as a circle if it is uniform.');
   }
 }
+
+Widget dashLine(BuildContext context,
+    {Color color,
+    double rootHeight,
+    double dashHeight,
+    double dashWidth,
+    double dashSpace}) {
+  dashSpace = dashSpace ?? 3;
+  dashHeight = dashHeight ?? 1;
+  dashWidth = dashWidth ?? 2;
+  rootHeight = rootHeight ?? dashHeight;
+  return CustomPaint(
+    // size：当child为null时，代表默认绘制区域大小，如果有child则忽略此参数，画布尺寸则为child尺寸
+    size: Size(double.infinity, rootHeight),
+    painter: DashPainter(
+        color: color,
+        rootHeight: rootHeight,
+        dashHeight: dashHeight,
+        dashSpace: dashSpace,
+        dashWidth: dashWidth),
+  );
+}
+
+class DashPainter extends CustomPainter {
+  final Color color;
+  final double rootHeight;
+  final double dashHeight;
+  final double dashSpace;
+  final double dashWidth;
+
+  DashPainter(
+      {this.color,
+      this.rootHeight,
+      this.dashHeight,
+      this.dashSpace,
+      this.dashWidth});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint() // 创建一个画笔并配置其属性
+      ..strokeWidth = this.dashHeight // 画笔的宽度
+      ..isAntiAlias = true // 是否抗锯齿
+      ..color = this.color ?? Color(0xffECECEC); // 画笔颜色
+
+    var max = size.width; // size获取到宽度
+    var dashWidth = this.dashWidth;
+    var dashSpace = this.dashSpace;
+    double startX = 0;
+    final space = (dashSpace + dashWidth);
+
+    while (startX < max) {
+      canvas.drawLine(Offset(startX, rootHeight / 2),
+          Offset(startX + dashWidth, rootHeight / 2), paint);
+      startX += space;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
