53f002d4574b255cfe38ceb94af27e96c6c9c7fe
kayoxu <371714752@qq.com>
2020-06-18 18:20:06 +0800

    优化ToolBar

--- lib/views/widget/base/keyboard/kayo_keyboard.dart
+++ lib/views/widget/base/keyboard/kayo_keyboard.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:kayo_package/views/widget/base/keyboard/utils/keyboard_manager_car_num.dart';
+import 'package:kayo_package/views/widget/base/keyboard/utils/keyboard_manager_number.dart';
 
 import 'keyboard.dart';
+import 'keyboard_common.dart';
 import 'utils/keyboard_manager.dart';
 
 class KayoKeyboard {
   static const CKTextInputType input =
       const CKTextInputType(name: 'KayoKeyboard');
+  static const CKTextInputType number =
+      const CKTextInputType(name: 'KayoKeyboard_number');
+  static const CKTextInputType carNum =
+      const CKTextInputType(name: 'KayoKeyboard_car_num');
 
   static double getHeight(BuildContext ctx) {
     MediaQueryData mediaQuery = MediaQuery.of(ctx);
@@ -22,6 +29,25 @@ class KayoKeyboard {
         KeyboardConfig(
             builder: (context, controller) {
               return Keyboard(controller: controller);
+            },
+            getHeight: KayoKeyboard.getHeight));
+    CoolKeyboardNumber.addKeyboard(
+        KayoKeyboard.number,
+        KeyboardConfig(
+            builder: (context, controller) {
+              return KeyboardCommon(
+                controller: controller,
+                kayoInputTypeCommon: KayoInputTypeCommon.number,
+              );
+            },
+            getHeight: KayoKeyboard.getHeight));
+    CoolKeyboardCarNum.addKeyboard(
+        KayoKeyboard.carNum,
+        KeyboardConfig(
+            builder: (context, controller) {
+              return KeyboardCommon(
+                  controller: controller,
+                  kayoInputTypeCommon: KayoInputTypeCommon.carNo);
             },
             getHeight: KayoKeyboard.getHeight));
   }
