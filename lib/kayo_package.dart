f8b82199c3092df928c44777aec3a73c9e947733
kayoxu <371714752@qq.com>
2020-07-29 10:49:12 +0800

    _index_utils

--- lib/kayo_package.dart
+++ lib/kayo_package.dart
@@ -3,6 +3,7 @@ library kayo_package;
 export 'package:kayo_package/extension/_index_extension.dart';
 export 'package:kayo_package/utils/_index_utils.dart';
 export 'package:kayo_package/views/_index_views.dart';
+export 'package:kayo_package/textInput_formatter/_index_text_input_formatter.dart';
 
 /// A Calculator.
 class Calculator {
