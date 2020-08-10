a108b5cee4bc1da32536a1571296d129209b587d
kayoxu <371714752@qq.com>
2020-08-10 17:58:07 +0800

    mvvm

--- lib/views/widget/tool_bar.dart
+++ lib/views/widget/tool_bar.dart
@@ -41,7 +41,7 @@ class ToolBar extends StatefulWidget {
     this.title = '',
     this.titelWidget,
     this.appBar,
-    this.iosBack = true,
+    this.iosBack = false,
     this.backClick,
     this.backgroundColor,
     this.appbarColor,
