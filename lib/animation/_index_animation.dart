5486040673d39f2415cb4d44730f37ba01d6627d
kayoxu <371714752@qq.com>
2020-08-13 16:06:10 +0800

    mvvm

---
+++ lib/animation/_index_animation.dart
@@ -0,0 +1,10 @@
+
+
+///
+///  kayo_package
+///  _index_animation.dart
+///
+///  Created by kayoxu on 2020/8/13 at 11:30 AM
+///  Copyright © 2020 kayoxu. All rights reserved.
+///
+
--- lib/kayo_package.dart
+++ lib/kayo_package.dart
@@ -3,6 +3,7 @@ library kayo_package;
 export 'package:kayo_package/extension/_index_extension.dart';
 export 'package:kayo_package/utils/_index_utils.dart';
 export 'package:kayo_package/views/_index_views.dart';
+export 'package:kayo_package/animation/_index_animation.dart';
 export 'package:kayo_package/textInput_formatter/_index_text_input_formatter.dart';
 export 'package:kayo_package/mvvm/_index_mvvm.dart';
 
