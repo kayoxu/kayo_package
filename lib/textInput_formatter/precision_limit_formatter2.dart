964603e08137fa1164a261150fecf6894a32f2cc
kayoxu <371714752@qq.com>
2020-08-21 16:50:49 +0800

    mvvm

---
+++ lib/textInput_formatter/precision_limit_formatter2.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/services.dart';
+
+///价格输入框和数量输入框的限制
+class PrecisionLimitFormatter2  extends TextInputFormatter {
+  static const defaultDouble = 0.001;
+  static double strToFloat(String str, [double defaultValue = defaultDouble]) {
+    try {
+      return double.parse(str);
+    } catch (e) {
+      return defaultValue;
+    }
+  }
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    String value = newValue.text;
+    int selectionIndex = newValue.selection.end;
+    if (value == ".") {
+      value = "0.";
+      selectionIndex++;
+    } else if (value != "" && value != defaultDouble.toString() && strToFloat(value, defaultDouble) == defaultDouble) {
+      value = oldValue.text;
+      selectionIndex = oldValue.selection.end;
+    }
+    return new TextEditingValue(
+      text: value,
+      selection: new TextSelection.collapsed(offset: selectionIndex),
+    );
+  }
+}
