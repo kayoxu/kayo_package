c4491af0e62ac899e0d57bfee0952577f46a90c6
kayoxu <371714752@qq.com>
2020-11-06 12:56:24 +0800

    card

--- lib/views/widget/base/text_with_drawable.dart
+++ lib/views/widget/base/text_with_drawable.dart
@@ -101,23 +101,7 @@ class TextWithDrawable extends StatelessWidget {
     } else if (onlyColumn) {
       child = onlyColumnWidget;
     } else {
-//      child = Row(
-//        mainAxisSize: MainAxisSize.min,
-//        crossAxisAlignment: CrossAxisAlignment.center,
-//        children: <Widget>[
-//          if (null != drawableStart) drawableStart,
-//          SizedBox(
-//              width: null == drawableStartPadding
-//                  ? drawablePadding
-//                  : drawableStartPadding),
-//          onlyColumnWidget,
-//          SizedBox(
-//              width: null == drawableEndPadding
-//                  ? drawablePadding
-//                  : drawableEndPadding),
-//          if (null != drawableEnd) drawableEnd,
-//        ],
-//      );
+
       child = onlyRowWidget;
     }
     return Container(
@@ -127,6 +111,178 @@ class TextWithDrawable extends StatelessWidget {
       margin: margin,
       decoration: decoration,
       child: child,
+    );
+  }
+}
+class ShowInfoRow extends StatelessWidget {
+  final String left;
+  final Widget leftWidget;
+  final String right;
+  final Widget rightWidget;
+  final int leftFlex;
+  final int rightFlex;
+  final Color bgColor;
+  final BorderRadiusGeometry borderRadius;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
+  final Color leftColor;
+  final Color rightColor;
+  final double leftFontSize;
+  final double rightFontSize;
+  final FontWeight leftFontWeight;
+  final FontWeight rightFontWeight;
+  final GestureTapCallback onTap;
+
+  ShowInfoRow({
+    this.left,
+    this.leftWidget,
+    this.right,
+    this.rightWidget,
+    this.leftFlex = 1,
+    this.rightFlex = 2,
+    this.bgColor = Colors.transparent,
+    this.borderRadius,
+    this.padding,
+    this.margin,
+    this.leftColor = const Color(0xff333333),
+    this.rightColor = const Color(0xff666666),
+    this.leftFontSize = 14,
+    this.rightFontSize = 14,
+    this.leftFontWeight = FontWeight.w600,
+    this.rightFontWeight = FontWeight.normal,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Widget view = Container(
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: borderRadius,
+      ),
+      child: Flex(
+        direction: Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            flex: leftFlex,
+            child: leftWidget ??
+                Text(
+                  left,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: leftColor,
+                    fontSize: leftFontSize,
+                    fontWeight: leftFontWeight,
+                  ),
+                ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            flex: rightFlex,
+            child: rightWidget ??
+                Text(
+                  right,
+                  textAlign: TextAlign.end,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: rightColor,
+                    fontSize: rightFontSize,
+                    fontWeight: rightFontWeight,
+                  ),
+                ),
+          ),
+        ],
+      ),
+    );
+    Widget child;
+    if (null == onTap) {
+      child = view;
+    } else {
+      child = InkWell(
+        onTap: onTap,
+        child: view,
+      );
+    }
+    return child;
+  }
+}
+class TextWithBg extends StatelessWidget {
+  final double width;
+  final double height;
+  final AlignmentGeometry alignment;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
+  final String text;
+  final double fontSize;
+  final Color fontColor;
+  final FontWeight fontWeight;
+  final TextAlign textAlign;
+  final String bgImg;
+  final Color bgColor;
+  final BoxBorder border;
+  final BorderRadiusGeometry borderRadius;
+  final List<BoxShadow> boxShadow;
+  final BoxShape shape;
+  final Gradient gradient;
+  final int maxLines;
+
+  TextWithBg({
+    this.width,
+    this.height,
+    this.alignment,
+    this.padding,
+    this.margin,
+    this.text = "",
+    this.fontSize = 14,
+    this.fontColor,
+    this.fontWeight,
+    this.textAlign,
+    this.bgImg,
+    this.bgColor,
+    this.border,
+    this.borderRadius,
+    this.boxShadow,
+    this.shape = BoxShape.rectangle,
+    this.gradient,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      alignment: alignment,
+      padding: padding,
+      margin: margin,
+      decoration: BoxDecoration(
+        image: null == bgImg
+            ? null
+            : DecorationImage(image: AssetImage(bgImg), fit: BoxFit.cover),
+        color: bgColor,
+        border: border,
+        borderRadius: borderRadius,
+        boxShadow: boxShadow,
+        shape: shape,
+        gradient: gradient,
+      ),
+      child: Text(
+        text,
+        maxLines: maxLines,
+        overflow: TextOverflow.ellipsis,
+        textAlign: textAlign,
+        style: TextStyle(
+          fontSize: fontSize,
+          color: fontColor,
+          fontWeight: fontWeight,
+        ),
+      ),
     );
   }
 }
