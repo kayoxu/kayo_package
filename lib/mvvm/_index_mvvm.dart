13a66776db079f8638f8ccc6ff75ba20f6651634
kayoxu <371714752@qq.com>
2021-03-24 09:33:52 +0800

    textv

--- lib/mvvm/_index_mvvm.dart
+++ lib/mvvm/_index_mvvm.dart
@@ -1,1 +1,5 @@
 export 'base/provider_widget.dart';
+export 'base/base_view_model.dart';
+export 'base/base_view_model_bus_event.dart';
+export 'base/base_view_model_list.dart';
+export 'base/base_view_model_refresh.dart';
