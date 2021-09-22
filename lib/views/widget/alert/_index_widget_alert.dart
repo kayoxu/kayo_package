390f0182686cf058488ebcd074aa477461a12fe6
kayoxu <371714752@qq.com>
2021-09-22 17:51:40 +0800

    隐私政策

--- lib/views/widget/alert/_index_widget_alert.dart
+++ lib/views/widget/alert/_index_widget_alert.dart
@@ -1,6 +1,7 @@
 export 'alert_center.dart';
 export 'alert_sheet.dart';
 export 'loading_dialog.dart';
+export 'privacy_dialog.dart';
 export 'flutter_cupertino_data_picker.dart';
 export 'datetime_picker/datetime_picker.dart';
 export 'datetime_picker_new/date_time_picker.dart';
