cb25872280a6b798e470e2ae0e75cde74d86b606
kayoxu <371714752@qq.com>
2021-10-27 19:13:14 +0800

    clickable.dart 增加宽度和高度

--- lib/extension/base_int_extension.dart
+++ lib/extension/base_int_extension.dart
@@ -3,23 +3,4 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:kayo_package/kayo_package.dart';
 
-extension BaseIntExtension on int? {
-  String toTimeStr({String? format, String? defaultTime}) {
-    try {
-      if (null != this) {
-        if ('${this!}'.length == 10) {
-          return BaseTimeUtils.timestampToTimeStr(this! * 1000, format: format);
-        } else if ('${this!}'.length == 13) {
-          return BaseTimeUtils.timestampToTimeStr(this!, format: format);
-        } else {
-          return BaseTimeUtils.timestampToTimeStr((this! - 60 * 60 * 0) * 1000,
-              format: format);
-        }
-      }
-      return defaultTime ?? KayoPackage.share.nullText;
-    } catch (e) {
-      print(e);
-      return defaultTime ?? KayoPackage.share.nullText;
-    }
-  }
-}
+extension BaseIntExtension on int? {}
--- lib/extension/base_num_extension.dart
+++ lib/extension/base_num_extension.dart
@@ -4,6 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:kayo_package/kayo_package.dart';
 
 extension BaseNumExtension on num? {
+  String toTimeStr({String? format, String? defaultTime}) {
+    try {
+      if (null != this) {
+        if ('${this!.round()}'.length == 10) {
+          return BaseTimeUtils.timestampToTimeStr(this!.round() * 1000,
+              format: format);
+        } else if ('${this!.round()}'.length == 13) {
+          return BaseTimeUtils.timestampToTimeStr(this!.round(),
+              format: format);
+        } else {
+          return BaseTimeUtils.timestampToTimeStr(
+              (this!.round() - 60 * 60 * 0) * 1000,
+              format: format);
+        }
+      }
+      return defaultTime ?? KayoPackage.share.nullText;
+    } catch (e) {
+      print(e);
+      return defaultTime ?? KayoPackage.share.nullText;
+    }
+  }
+
   Radius toRadius() {
     if (null != this) {
       return Radius.circular(this! + 0.0);
