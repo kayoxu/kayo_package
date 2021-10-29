dedbd8ae6085db8d8679d538682aad2535dcfaac
kayoxu <371714752@qq.com>
2021-10-29 12:49:28 +0800

    clickable.dart 增加宽度和高度

--- lib/http/test/base_api_test.dart
+++ lib/http/test/base_api_test.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:kayo_package/http/base_api.dart';
-
-///
-///  kayo_package
-///  base_api_test.dart
-///
-///  Created by kayoxu on 2021/8/5 at 2:29 下午
-///  Copyright © 2021 kayoxu. All rights reserved.
-///
-
-class BaseApiTest extends BaseAPI {
-  @override
-  String get host => isEnv ? '' : '';
-
-  @override
-  String get hostFile => isEnv ? '' : '';
-
-  @override
-  String get hostSocket => isEnv ? '' : '';
-
-  @override
-  init() {}
-
-  @override
-  bool get isEnv => true;
-}
+// import 'package:flutter/material.dart';
+// import 'package:kayo_package/http/base_api.dart';
+//
+// ///
+// ///  kayo_package
+// ///  base_api_test.dart
+// ///
+// ///  Created by kayoxu on 2021/8/5 at 2:29 下午
+// ///  Copyright © 2021 kayoxu. All rights reserved.
+// ///
+//
+// class BaseApiTest extends BaseAPI {
+//   @override
+//   String get host => isEnv ? '' : '';
+//
+//   @override
+//   String get hostFile => isEnv ? '' : '';
+//
+//   @override
+//   String get hostSocket => isEnv ? '' : '';
+//
+//   @override
+//   init() {}
+//
+//   @override
+//   bool get isEnv => true;
+// }
