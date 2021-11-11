86ca764a175a388ea47c3ebd0ad03749d009f82b
kayoxu <371714752@qq.com>
2021-11-11 14:34:44 +0800

    clickable.dart 增加宽度和高度

--- lib/utils/base_sys_utils.dart
+++ lib/utils/base_sys_utils.dart
@@ -225,9 +225,33 @@ class BaseSysUtils {
   }
 
   static Color randomColor() {
-    var random = Random();
-    var index = random.nextInt(3);
-    return getLockColor(index, hasNext: false);
+    // var random = Random();
+    // var index = random.nextInt(3);
+    // return getLockColor(index, hasNext: false);
+    return _getRandomColor();
+  }
+
+
+  static Color _getRandomColor({int r = 255, int g = 255, int b = 255, a = 255}) {
+    if (r == 0 || g == 0 || b == 0) return Colors.black;
+    if (a == 0) return Colors.white;
+    return Color.fromARGB(
+      a,
+      r != 255 ? r : Random.secure().nextInt(r),
+      g != 255 ? g : Random.secure().nextInt(g),
+      b != 255 ? b : Random.secure().nextInt(b),
+    );
+  }
+
+
+  static Color indexColor(int index) {
+    switch (index) {
+      case 0:
+        return Color(0xffFF940E);
+
+      default:
+        return randomColor();
+    }
   }
 
   /// 颜色创建方法
