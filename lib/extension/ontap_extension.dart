9e649f053fec532ebf2e414e7e1514b0dfc0f6ce
kayoxu <371714752@qq.com>
2023-03-14 11:42:24 +0800

    addFittedBox2`

--- lib/extension/ontap_extension.dart
+++ lib/extension/ontap_extension.dart
@@ -6,8 +6,8 @@ import '../kayo_package_utils.dart';
 import 'base_build_context_extension.dart';
 
 extension OnTapExtension on void Function()? {
-  void Function() safeTap({int? time = 500}) {
+  void Function() safeTap({int? time = 500, Function()? onSafe}) {
     if (null == this) return this ?? () {};
-    return BaseSysUtils.safeTap(this!, time: time);
+    return BaseSysUtils.safeTap(this!, time: time, onSafe: onSafe);
   }
 }
