554fe8cc1a294b827f077a28967b5cc0d77b9a67
kayoxu <371714752@qq.com>
2023-03-28 17:59:08 +0800

    addFittedBox2`

--- lib/utils/base_sys_utils.dart
+++ lib/utils/base_sys_utils.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
-import 'dart:typed_data';
 import 'dart:ui';
 import 'package:convert/convert.dart';
 import 'package:crypto/crypto.dart';
@@ -423,117 +422,17 @@ class BaseSysUtils {
       return '.log';
     } else if (fileExt.endsWith(".in")) {
       return '.in';
+    } else if (fileExt.endsWith(".pdf")) {
+      return '.pdf';
+    } else if (fileExt.endsWith(".doc")) {
+      return '.doc';
+    } else if (fileExt.endsWith(".xls")) {
+      return '.xls';
+    } else if (fileExt.endsWith(".xlsx")) {
+      return '.xlsx';
     } else {
       return null;
     }
-  }
-
-  static List<int> hex2ListInt(String? hex) {
-    if (hex is! String) {
-      throw 'Expected string containing hex digits';
-    }
-    if (hex.length % 2 != 0) {
-      throw 'Odd number of hex digits';
-    }
-    var l = hex.length ~/ 2;
-    var result = Uint8List(l);
-    for (var i = 0; i < l; ++i) {
-      var x = int.parse(hex.substring(i * 2, (2 * (i + 1))), radix: 16);
-      if (x.isNaN) {
-        throw 'Expected hex string';
-      }
-      result[i] = x;
-    }
-    return result;
-  }
-
-  static String listInt82Hex(List<int>? byteArr) {
-    if (byteArr == null || byteArr.isEmpty) {
-      return "";
-    }
-    Uint8List result = Uint8List(byteArr.length << 1);
-    var hexTable = [
-      '0',
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F'
-    ]; //16进制字符表
-    for (var i = 0; i < byteArr.length; i++) {
-      var bit = byteArr[i]; //取传入的byteArr的每一位
-      var index = bit >> 4 & 15; //右移4位,取剩下四位
-      var i2 = i << 1; //byteArr的每一位对应结果的两位,所以对于结果的操作位数要乘2
-      result[i2] = hexTable[index].codeUnitAt(0); //左边的值取字符表,转为Unicode放进resut数组
-      index = bit & 15; //取右边四位
-      result[i2 + 1] =
-          hexTable[index].codeUnitAt(0); //右边的值取字符表,转为Unicode放进resut数组
-    }
-    return String.fromCharCodes(result); //Unicode转回为对应字符,生成字符串返回
-  }
-
-  static Uint8List hex2Uint8List(String? hex) {
-    if (hex is! String) {
-      throw 'Expected string containing hex digits';
-    }
-    if (hex.length % 2 != 0) {
-      throw 'Odd number of hex digits';
-    }
-    var l = hex.length ~/ 2;
-    var result = Uint8List(l);
-    for (var i = 0; i < l; ++i) {
-      var x = int.parse(hex.substring(i * 2, (2 * (i + 1))), radix: 16);
-      if (x.isNaN) {
-        throw 'Expected hex string';
-      }
-      result[i] = x;
-    }
-    return result;
-  }
-
-  static String uint82Hex(Uint8List? byteArr) {
-    if (byteArr == null || byteArr.isEmpty) {
-      return "";
-    }
-    Uint8List result = Uint8List(byteArr.length << 1);
-    var hexTable = [
-      '0',
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F'
-    ]; //16进制字符表
-    for (var i = 0; i < byteArr.length; i++) {
-      var bit = byteArr[i]; //取传入的byteArr的每一位
-      var index = bit >> 4 & 15; //右移4位,取剩下四位
-      var i2 = i << 1; //byteArr的每一位对应结果的两位,所以对于结果的操作位数要乘2
-      result[i2] = hexTable[index].codeUnitAt(0); //左边的值取字符表,转为Unicode放进resut数组
-      index = bit & 15; //取右边四位
-      result[i2 + 1] =
-          hexTable[index].codeUnitAt(0); //右边的值取字符表,转为Unicode放进resut数组
-    }
-    return String.fromCharCodes(result); //Unicode转回为对应字符,生成字符串返回
   }
 
   static Function() safeTap(Function() fn,
