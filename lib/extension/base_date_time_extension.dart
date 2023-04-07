867cabb8ae2df892d2e56a6fbc05298e017c23cd
kayoxu <371714752@qq.com>
2023-04-07 15:26:10 +0800

    addFittedBox2`

--- lib/extension/base_date_time_extension.dart
+++ lib/extension/base_date_time_extension.dart
@@ -4,11 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:kayo_package/kayo_package.dart';
 
 extension BaseDateTimeExtension on DateTime? {
-  String toTimeStr({String? format, String? defaultTime}) {
+  String toTimeStr({String? format, String? defaultTime, bool tz = false}) {
     try {
       if (null != this) {
         var seconds = this!.millisecondsSinceEpoch;
-        return BaseTimeUtils.timestampToTimeStr(seconds, format: format);
+        if (tz) {
+          var replaceAll = BaseTimeUtils.timestampToTimeStr(
+              seconds, format: format).replaceAll(" ", "T");
+          return "${replaceAll}Z";
+        } else {
+          return BaseTimeUtils.timestampToTimeStr(seconds, format: format);
+        }
       }
       return defaultTime ?? KayoPackage.share.nullText;
     } catch (e) {
